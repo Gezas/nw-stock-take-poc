867e2c5debb69326a01bf0079da6521aa4c2d643
Deividas <deividasjac@gmail.com>
2023-02-11 12:42:06 +0000

    initial project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Stock take app
+    
+    </div>
+  );
+}
+
+export default App;
